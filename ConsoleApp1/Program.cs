c6b5e47bdade226196fac53e13e4567e1039c2c8
Magel <magelvictoria@gmail.com>
2020-08-09 17:04:36 +0300

    Новая фича

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет!");
+            Console.WriteLine("Привет, новая фича!");
         }
     }
 }
